cb1e3e99043a286ff24b5adfb9a58efc2282c425
c0c2505276 <c0c2505276@edu.teu.ac.jp>
2026-04-21 14:55:14 +0900

    練習４：GAME OVER

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -47,6 +47,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("GAME OVER")
+            return #GEME OVER
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
